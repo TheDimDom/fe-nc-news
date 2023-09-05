12add05cfa1c9244f23534f0caab6d3724411485
George Samuels <dominic.g.samuels@outlook.com>
2023-09-05 14:24:48 +0100

    updated from feedback

--- src/Components/ArticleDetail.jsx
+++ src/Components/ArticleDetail.jsx
@@ -5,32 +5,43 @@ import { getArticleById } from "../assets/Requests/api";
 const ArticleDetail = () => {
   const { articleId } = useParams();
   const [article, setArticle] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     getArticleById(articleId)
       .then((response) => {
         setArticle(response.data);
+        setIsLoading(false);
       })
       .catch((error) => {
-        console.log("Error fetching article", error);
+        setIsLoading(false);
+        setIsError(true);
       });
   }, [articleId]);
 
-  if (!article) {
-    return <p>Loading...</p>;
-  }
-
   return (
     <div className="ArticleDetail">
-      <h2>{article.title}</h2>
-      {article.article_img_url && (
-        <img src={article.article_img_url} alt={`Image for ${article.title}`} />
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Something went wrong!</p>}
+
+      {article && (
+        <>
+          <h2>{article.title}</h2>
+          {article.article_img_url && (
+            <img
+              src={article.article_img_url}
+              alt={`Image for ${article.title}`}
+            />
+          )}
+          <p>Topic: {article.topic}</p>
+          <p>Author: {article.author}</p>
+          <p>{article.body}</p>
+          <p>Created_at: {article.created_at}</p>
+          <p>Votes: {article.votes}</p>
+        </>
       )}
-      <p>Topic: {article.topic}</p>
-      <p>Author: {article.author}</p>
-      <p>{article.body}</p>
-      <p>Created_at: {article.created_at}</p>
-      <p>Votes: {article.votes}</p>
     </div>
   );
 };
