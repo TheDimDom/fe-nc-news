777bb74371fed43fe110fc80997b4e78ccfd85e6
George Samuels <dominic.g.samuels@outlook.com>
2023-09-05 15:30:47 +0100

    added comment articles

--- src/Components/ArticleDetail.jsx
+++ src/Components/ArticleDetail.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { getArticleById } from "../assets/Requests/api";
+import { getArticleById, getCommentsByArticleId } from "../assets/Requests/api";
+import CommentCard from "./CommentCard";
 
 const ArticleDetail = () => {
   const { articleId } = useParams();
   const [article, setArticle] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     setIsLoading(true);
@@ -19,6 +21,14 @@ const ArticleDetail = () => {
         setIsLoading(false);
         setIsError(true);
       });
+  }, [articleId]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getCommentsByArticleId(articleId).then((response) => {
+      setComments(response.data);
+      setIsLoading(false);
+    });
   }, [articleId]);
 
   return (
@@ -42,8 +52,16 @@ const ArticleDetail = () => {
           <p>Votes: {article.votes}</p>
         </>
       )}
+
+      {comments && comments.length > 0 && (
+        <>
+          <h3 className="CommentsTitle">Comments</h3>
+          {comments.map((comment) => (
+            <CommentCard comment={comment} key={comment.comment_id} />
+          ))}
+        </>
+      )}
     </div>
   );
 };
-
 export default ArticleDetail;
